2016e7c314c9e2cd5d29b80dad359272b16f18e6
Ethan Koenig <ethantkoenig@gmail.com>
2016-08-29 12:27:07 -0400

    First implementation
    Still missing support for some USFM features (tables, images, cross-references)

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+
+from html.render_html import HtmlVisitor
+from usfm import lex, parse
+
+
+def main(input_filename, output_filename):
+    with open(input_filename, "r") as input_file:
+        contents = input_file.read()
+        lexer = lex.UsfmLexer.create()
+        lexer.input(contents)
+        parser = parse.UsfmParser.create()
+        document = parser.parse(lexer=lexer)
+        with open(output_filename, "w") as output_file:
+            visitor = HtmlVisitor(output_file)
+            visitor.write(document)
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2])
